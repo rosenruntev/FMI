03f788f4928e97303aa5c4189675c5013b88ded8
Rosen Runtev <runtev.rosen@gmail.com>
2019-01-23 09:38:34 +0200

    Added PerfectNumber - a program that checks if number is perfect

---
+++ Homeworks/Object-oriented Programming/PerfectNumber/src/EntryPoint.java
@@ -0,0 +1,23 @@
+/* Write a java program that checks if number is perfect */
+import java.util.Scanner;
+
+public class EntryPoint {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter number:");
+        int number = input.nextInt();
+
+        int sumOfDividers = 0;
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                sumOfDividers += i;
+            }
+        }
+
+        if (number == sumOfDividers) {
+            System.out.println("Number is perfect");
+        } else {
+            System.out.println("Number is not perfect");
+        }
+    }
+}
